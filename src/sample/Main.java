10e02db0a7143fe674f1f1d3f0dbf3a0bd6b4c33
alviancode <alvianwijaya200@gmail.com>
2020-12-08 00:44:53 +0700

    Massive bug fixes, add new feature, tidy up query, tidy up code

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,16 +8,15 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 800, 630));
-        primaryStage.show();
-    }
-
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        primaryStage.setTitle("Ultimate Movie Rating");
+        primaryStage.setScene(new Scene(root, 800, 630));
+        primaryStage.show();
     }
 }
